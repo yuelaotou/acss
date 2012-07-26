468ca2588190e146cfa3b0f0795068815ab56c49
yangguang <yuelaotou@gmail.com>
2012-07-26 15:10:02 +0800

    修改程序运行根目录为C盘，防止有人的机器只有一个盘符。
    Signed-off-by: yangguang <yuelaotou@gmail.com>

--- src/com/kd/acss/consts/Consts.java
+++ src/com/kd/acss/consts/Consts.java
@@ -12,42 +12,32 @@ public final class Consts {
 	/**
 	 * 设置属性保存的位置，例如：上下班时间
 	 */
-	public final static String PATH_PROPERTIES = "D:\\acss\\info\\配置文件.properties";
-
-	/**
-	 * 节假日文件的保存位置
-	 */
-	public final static String PATH_VACATIONS = "D:\\acss\\info\\节假日.txt";
-
-	/**
-	 * 串休日文件的保存位置
-	 */
-	public final static String PATH_WORKDAYS = "D:\\acss\\info\\串休日.txt";
+	public final static String PATH_PROPERTIES = "C:\\acss\\info\\配置文件.properties";
 
 	/**
 	 * 个人出差登记表保存路径
 	 */
-	public final static String FOLDER_EVECTIONS = "D:\\acss\\evection";
+	public final static String FOLDER_EVECTIONS = "C:\\acss\\evection";
 
 	/**
 	 * 假期维护表的保存位置
 	 */
-	public final static String PATH_HOLIDAYS = "D:\\acss\\info\\假期维护表.xlsx";
+	public final static String PATH_HOLIDAYS = "C:\\acss\\info\\假期维护表.xlsx";
 
 	/**
 	 * 职工基本信息表保存位置
 	 */
-	public final static String PATH_EMPLOYEEBASE = "D:\\acss\\info\\职工基本信息表.xlsx";
+	public final static String PATH_EMPLOYEEBASE = "C:\\acss\\info\\职工基本信息表.xlsx";
 
 	/**
 	 * 考勤明细表保存位置
 	 */
-	public final static String PATH_EMPLOYEEDETAIL = "D:\\acss\\考勤详细信息表.xlsx";
+	public final static String PATH_EMPLOYEEDETAIL = "C:\\acss\\考勤详细信息表.xlsx";
 
 	/**
 	 * 考勤统计总表保存位置
 	 */
-	public final static String PATH_EMPLOYEETOTAL = "D:\\acss\\考勤统计总表.xlsx";
+	public final static String PATH_EMPLOYEETOTAL = "C:\\acss\\考勤统计总表.xlsx";
 
 	/**
 	 * 日期正则表达式 yyyy-MM-dd，满足基本判断
